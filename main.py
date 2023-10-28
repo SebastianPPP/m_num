b82ce5e20fccd76603a9080babe7a9174c9cd481
Sebastian <sebastianparzych66@gmail.com>
2023-10-28 17:25:27 +0200

    Test commit

--- main.py
+++ main.py
@@ -3,18 +3,28 @@ import matplotlib as plt
 
 #Metody iteracyjne [Gauss-Seidel]
 
-def Gauss_Seidel(macierz_A, macierz_B):
+def Gauss_Seidel(A, B):
     #elementy na głównej przekątnej nie mogą być zerowe
     #mamy macierz z num_eq wierszami i num_of_x niewiadomymi
     #zakładamy zerowy wektor początkowy x=[0 0 0 ...]^T
+    num_of_rows = A.shape[0]
+    X = np.zeros(num_of_rows)
+    for j in range(1):
+        for k in range(num_of_rows):
+            sum = 0
+            for l in range(num_of_rows):
+                if l != k:
+                    sum += A[k, l] * X[l]
+            X[k] = (B[k]-sum)/A[k,k]
+    return X.T
 
-
-
-macierz_A = np.array([
+A = np.array([
     [3, -0.1, -0.2],
     [0.1, 7, -0.3],
-    [0.3, 0.2, 10]
+    [0.3, -0.2, 10]
 ])
-macierz_B = np.array([7.85, -19.3, 71.4])
 
-print(macierz_A)
+B = np.array([7.85, -19.3, 71.4])
+B1 = B.T
+
+print(Gauss_Seidel(A,B1))
